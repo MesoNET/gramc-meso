39891ebcc06802112881f97b6d76f91416dd2faf
Emmanuel Courcelle <emmanuel.courcelle@toulouse-inp.fr>
2023-06-19 14:25:22 +0200

    Commande CronCmd permettant d'appeler Cron par une commande et pas seulement par une fonction d'API

---
+++ src/Command/CronCmd.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * This file is part of GRAMC (Computing Ressource Granting Software)
+ * GRAMC stands for : Gestion des Ressources et de leurs Attributions pour Mésocentre de Calcul
+ *
+ * GRAMC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  GRAMC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRAMC.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  authors : Emmanuel Courcelle - C.N.R.S. - UMS 3667 - CALMIP
+ *            Nicolas Renon - Université Paul Sabatier - CALMIP
+ **/
+
+/***************************
+ *
+ * Appel de la fonction de maintenance Cron::execute
+ *
+ * On peut aussi exécuter les cron en utilisant l'api
+ * cf. src/Conteroller/Adminuxcontroller
+ *
+ * UTILISATION:
+ *
+ *      bin/console app:cron
+ *
+ **************************************************/
+
+namespace App\Command;
+
+use App\GramcServices\Cron\Cron;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Doctrine\ORM\EntityManagerInterface;
+
+class CronCmd extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:cron';
+
+    public function __construct(private Cron $sc)
+    {
+        // best practices recommend to call the parent constructor first and
+        // then set your own properties. That wouldn't work in this case
+        // because configure() needs the properties set in this constructor
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Exécution des tâches de maintenance');
+        $this->setHelp('Voir src/GramcServices/Cron pour les détails');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // this method must return an integer number with the "exit status code"
+        // of the command.
+
+        // return this if there was no problem running the command
+
+        $sc = $this->sc;
+        $sc->execute();
+        return 0;
+    }
+}
