4c673da7c24e70e530e78d5ba37c29d9b65607d9
Saladeuh <emilienmonet10@gmail.com>
2024-01-16 10:28:30 +0100

    test(codecep): génération du cest availability

---
+++ tests/Application/AvailabilityCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Application;
+
+use App\Tests\ApplicationTester;
+
+class AvailabilityCest
+{
+    public function _before(ApplicationTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ApplicationTester $I)
+    {
+    }
+}
