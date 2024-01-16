6b1f31727a0e0445782a724eb472c9915d9f5109
Saladeuh <emilienmonet10@gmail.com>
2024-01-16 11:34:02 +0100

    config(codecept): configurations générales

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,10 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
+
+// execute le composer db pour charger la bd
+passthru(sprintf(
+    'APP_ENV=%s php "%s/../bin/console" cache:clear --no-warmup',
+    $_ENV['APP_ENV'],
+    __DIR__
+));
