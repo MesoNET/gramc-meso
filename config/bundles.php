2a20ef831e2503819e4dcd8c9d16786b07074541
Saladeuh <emilienmonet10@gmail.com>
2024-01-26 11:25:14 +0100

    feat(project): ajout d'api-platform

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
