18ed4abed67731162c38ae5b80f14a1d419ae20d
Saladeuh <emilienmonet10@gmail.com>
2024-02-01 16:20:08 +0100

    config(project): application de certaines recipies des packages pour mettre à jour les configs

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\UX\TogglePassword\TogglePasswordBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
