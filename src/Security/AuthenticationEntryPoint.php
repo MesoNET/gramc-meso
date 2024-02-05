88fe34a118c907323c4203889b4906bdd41380b6
bill0076 <benjamin.billiau@univ-reims.fr>
2024-02-05 10:29:43 +0100

    feat(back): ajout de redirection en cas de non droit

---
+++ src/Security/AuthenticationEntryPoint.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Security;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
+{
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+    ) {
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null): RedirectResponse
+    {
+        $request->getSession()->getFlashBag()->add('note', 'You have to login in order to access this page.');
+
+        return new RedirectResponse($this->urlGenerator->generate('accueil'));
+    }
+}
