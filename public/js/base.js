8b2d32cbd06b9fbc05b3f3b7cc0394feb762b67e
bill0076 <benjamin.billiau@univ-reims.fr>
2024-02-06 09:27:04 +0100

    feat(front): ajout d'un script pour selectioné une ligne du tableau

--- public/js/base.js
+++ public/js/base.js
@@ -18,6 +18,11 @@ $( document ).ready(function() {
     })
 })
 
+$(document).ready(function(){
+    $('*[data-href]').click(function(){
+        window.location = $(this).data('href');
+    });
+});
 
 $( document ).ready(function() {
     //Récupérer "En voir plus..." 
