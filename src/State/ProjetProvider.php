3c84ad2255a162fde08f0dd3fa24060899c0aab9
Saladeuh <emilienmonet10@gmail.com>
2024-02-12 14:02:48 +0100

    feat(api): ajout du get projet et du projetprovider

--- src/State/ProjetProvider.php
+++ src/State/ProjetProvider.php
@@ -8,7 +8,7 @@ use App\Entity\Serveur;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 
-final class IndividuProvider implements ProviderInterface
+final class ProjetProvider implements ProviderInterface
 {
     public function __construct(
         private ProviderInterface $itemProvider,
@@ -18,16 +18,16 @@ final class IndividuProvider implements ProviderInterface
     }
 
     /**
-     * Fournit l'individu ayant des users sur le serveur connecté.
+     * Fournit le projet ayant des users sur le serveur connecté.
      *
      * @return array|object|object[]|null
      */
-    public function provide(Operation $operation, array $uriVariables = [], array $context = []): null|array|object
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): array|object|null
     {
-        $individu = $this->itemProvider->provide($operation, $uriVariables, $context);
+        $projet = $this->itemProvider->provide($operation, $uriVariables, $context);
         $serveur = $this->entityManager->getRepository(Serveur::class)->findOneBy(['admname' => $this->security->getUser()->getUserIdentifier()]);
-        if (array_intersect($individu->getUser()->toArray(), $serveur->getUser()->toArray())) {
-            return $individu;
+        if (array_intersect($projet->getUser()->toArray(), $serveur->getUser()->toArray())) {
+            return $projet;
         } else {
             return null;
         }
