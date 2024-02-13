39a6d5aa6ba49c9e41bd4129b810f792b6048ae0
Saladeuh <emilienmonet10@gmail.com>
2024-02-13 11:09:19 +0100

    feat(api): ajout du get pour les projets

--- src/State/ProjetProvider.php
+++ src/State/ProjetProvider.php
@@ -4,6 +4,7 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
+use App\Entity\Projet;
 use App\Entity\Serveur;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
