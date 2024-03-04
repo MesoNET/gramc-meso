c5300fa4caa830a42115dc94e0aa5035c0f4bc64
bill0076 <benjamin.billiau@univ-reims.fr>
2024-03-04 16:54:23 +0100

    fix(front): fix du front

---
+++ src/Form/EtablissementSearchType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EtablissementSearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(
+                'nomEtablissement',
+                TextType::class,
+                [
+                    'label' => 'Filtre:',
+                    'attr' => ['size' => '50'],
+                    'required' => false,
+                ]
+            )
+            ->add(
+                'filtrer',
+                SubmitType::class,
+                [
+                    'label' => 'Filtrer',
+                ]
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
