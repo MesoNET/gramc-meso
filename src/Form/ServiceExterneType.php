9e5f1501cf24912bf17f18145ac18c1cb2fcd56d
Saladeuh <emilienmonet10@gmail.com>
2024-03-25 14:00:15 +0100

    feat(front): ajout de la gestion des services externes

---
+++ src/Form/ServiceExterneType.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * This file is part of GRAMC (Computing Ressource Granting Software)
+ * GRAMC stands for : Gestion des Ressources et de leurs Attributions pour Mésocentre de Calcul.
+ *
+ * GRAMC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  GRAMC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRAMC.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  authors : Miloslav Grundmann - C.N.R.S. - UMS 3667 - CALMIP
+ *            Emmanuel Courcelle - C.N.R.S. - UMS 3667 - CALMIP
+ *            Nicolas Renon - Université Paul Sabatier - CALMIP
+ **/
+
+namespace App\Form;
+
+use App\Entity\Serveur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ServeurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, ['required' => true, 'label' => 'nom du serveur *:', 'disabled' => $options['modifier']])
+            ->add('desc', TextType::class, ['required' => false, 'label' => 'Description :'])
+            ->add('cguUrl', TextType::class, ['required' => false, 'label' => 'Url des CGU :'])
+            ->add('admname', TextType::class, ['required' => false, 'label' => 'Nom d\'utilisateur pour l\'API :'])
+            ->add('formatSSH', ChoiceType::class, ['choices' => Serveur::SSH, 'choice_translation_domain' => 'serveur',
+                'multiple' => true, 'expanded' => true, 'required' => true, 'label' => 'format de clé ssh'])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                    'toggle' => true,
+                ],
+                'first_options' => [
+                    'hash_property_path' => 'password',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez entrer un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                    'label' => 'Mot de passe API',
+                    'toggle' => true,
+                ],
+                'second_options' => [
+                    'label' => 'Réécrivez le mot de passe',
+                ],
+                'invalid_message' => 'Les champs doivent correspondre.',
+                'mapped' => false,
+            ]);
+        if ($options['modifier']) {
+            $builder
+                ->add('submit', SubmitType::class, ['label' => 'modifier'])
+                ->add('reset', ResetType::class, ['label' => 'reset']);
+        } elseif ($options['ajouter']) {
+            $builder
+                ->add('submit', SubmitType::class, ['label' => 'ajouter'])
+                ->add('reset', ResetType::class, ['label' => 'reset']);
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => 'App\Entity\Serveur',
+                'modifier' => false,
+                'ajouter' => false,
+            ]
+        );
+    }
+}
